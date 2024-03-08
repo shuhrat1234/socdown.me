883adab52cd0704e9a66a120a39ede17daacb302
Shukhrat <shukrillaevshukhrat@gmail.com>
2024-03-08 23:19:34 +0500

    translate

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const select = document.querySelector('select');
+const allLang = ['en', 'ru', 'uz'];
+
+
+select.addEventListener('change', changeURLLanguage);
+
+function changeURLLanguage() {
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+function changeLanguage() {
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+    console.log(hash);
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#en';
+        location.reload();
+    }
+    select.value = hash;
+    document.querySelector('title').innerHTML = langArr['unit'][hash];
+    for (let key in langArr) {
+        let elem = document.querySelector('.lng-' + key);
+        if (elem) {
+            elem.innerHTML = langArr[key][hash];
+        }
+
+    }
+}
+
+changeLanguage();
---
+++ js/lang.js
@@ -0,0 +1,13 @@
+const langArr = {
+    "unit" :  {
+        "en": "Instagram Video Download",
+        "ru": "Скачать видео из Инстаграма",
+        "uz": "instagram video yuklab olish",
+    }, 
+    "chip": {
+        "en": "Download Video Instagram, Photo, Reels, Stories, IGTV online",
+        "ru": "Скачать видео Instagram, фото, ролики, истории, IGTV онлайн",
+        "uz": "Instagram videolari, fotosuratlari, videolari, hikoyalari, IGTV-ni onlayn yuklab oling",
+    }, 
+
+}
